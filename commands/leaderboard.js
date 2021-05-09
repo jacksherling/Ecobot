e175ac2418682ba14e9b963a81d7c5edfcc73821
jacksherl <65086686+jacksherl@users.noreply.github.com>
2021-05-09 10:45:45 -0400

    make leaderboard show top users by tier

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -3,18 +3,37 @@ const { command, Server } = require("./command");
 
 const leaderboard = new command(
     "leaderboard",
-    "Displays top 10 members with the most money.",
+    "Displays top 5 members with the most money and highest tiers.",
     "leaderboard",
     async (msg, author, server, words) => {
-        const topMembers = server.members
+        const topMembersByBalance = server.members
             .sort((a, b) => b.balance - a.balance)
-            .slice(0, 10);
+            .slice(0, 5);
+        const topMembersByTier = server.members
+            .sort((a, b) => b.tier - a.tier)
+            .slice(0, 5);
         genEmbed(msg.channel, `${msg.guild.name} Leaderboards`, (embed) => {
+            embed.addFields({
+                name: "--- BALANCE ---",
+                value: "The top users by balance.",
+            });
             embed.addFields(
-                topMembers.map((v) => {
+                topMembersByBalance.map((v) => {
                     return {
                         name: v.name,
-                        value: v.balance,
+                        value: "$" + v.balance,
+                    };
+                })
+            );
+            embed.addFields({
+                name: "--- TIER ---",
+                value: "The top users by tier.",
+            });
+            embed.addFields(
+                topMembersByTier.map((v) => {
+                    return {
+                        name: v.name,
+                        value: "Tier " + v.tier,
                     };
                 })
             );
